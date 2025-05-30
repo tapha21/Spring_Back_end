4e47d8e6ed4f847d9906b43baf317596f3ad2360
Tapha21 <tvphvtall@gmail.com>
2025-05-30 00:20:47 +0000

    Correction du liens angular

--- src/main/java/com/ges_abs/config/CorsConfig.java
+++ src/main/java/com/ges_abs/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // Autorise toutes les routes sous /api
+        registry.addMapping("/**") // Autorise toutes les routes sous /api
                 .allowedOrigins(
                         "https://angular-hackaton.vercel.app/",
                         "https://angular-hackaton-lkingtxq6-victorin-attolodes-projects.vercel.app/"
