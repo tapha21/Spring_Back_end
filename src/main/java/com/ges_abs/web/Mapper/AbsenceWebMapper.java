42da51c40278f8ebc5562725ea8229ecde5c8b14
Tapha21 <tvphvtall@gmail.com>
2025-06-12 23:14:48 +0000

    Ajout  response evenement pour les justificatifs

--- src/main/java/com/ges_abs/web/Mapper/AbsenceWebMapper.java
+++ src/main/java/com/ges_abs/web/Mapper/AbsenceWebMapper.java
@@ -12,7 +12,7 @@ import org.mapstruct.factory.Mappers;
 
 public interface AbsenceWebMapper {
     AbsenceWebMapper INSTANCE = Mappers.getMapper(AbsenceWebMapper.class);
-//    @Mapping(source = "justificatifImage", target = "justificatifImage")
+   @Mapping(source = "justificatifImage", target = "justificatifImage")
 @Mapping(source = "session", target = "session")
 AbsenceWebResponseDto toDto(Evenement absence);
     Evenement ToEntity(AbsenceWebRequestDto userDto);
