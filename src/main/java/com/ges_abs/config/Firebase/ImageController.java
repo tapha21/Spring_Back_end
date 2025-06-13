96afd8d329f38993b6b71fbc289afa0e6b1a1cf5
Safietou2003 <167894923+Safietou2003@users.noreply.github.com>
2025-06-13 21:16:22 +0000

    Update ImageController.java

--- src/main/java/com/ges_abs/config/Firebase/ImageController.java
+++ src/main/java/com/ges_abs/config/Firebase/ImageController.java
@@ -1,12 +1,12 @@
 package com.ges_abs.config.Firebase;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import io.jsonwebtoken.io.IOException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/images")
@@ -18,13 +18,19 @@ public class ImageController {
         this.imageService = imageService;
     }
 
-    @PostMapping("/upload")
-    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) throws java.io.IOException {
+    @Operation(
+            summary = "Uploader une image"
+    )
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadImage(
+            @Parameter(description = "Fichier à uploader")
+            @RequestParam("file") MultipartFile file
+    ) {
         try {
             String imageUrl = imageService.uploadFile(file);
             return ResponseEntity.ok(imageUrl);
-        } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Upload failed");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Upload failed: " + e.getMessage());
         }
     }
 }
