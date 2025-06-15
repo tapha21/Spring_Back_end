d4c1a4cbd14c0db43acb236bc03f2f991afc9a08
TORINO <victorinattolode@gmail.com>
2025-06-15 17:15:38 +0000

    Commentaire justificatifImage

--- src/main/java/com/ges_abs/web/dto/response/AbsenceWebResponseDto.java
+++ src/main/java/com/ges_abs/web/dto/response/AbsenceWebResponseDto.java
@@ -17,7 +17,7 @@ public class AbsenceWebResponseDto {
     private LocalTime heureDebut;
     private LocalTime heureFin;
     private String justification;
-    private List<String> justificatifImage;
+    //private List<String> justificatifImage;
     private Etat etat;
     private Type type;
     private EtudiantSimpleWebResponseDto etudiant;
